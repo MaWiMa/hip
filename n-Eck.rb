14183f04f119573b2c52c7cdb26720cdf2f4ba51
MaWiMa <WilliV109@gMail.com>
2015-08-09 00:32:24 +0200

    geändert:       images/polygon-angle.dxf 	geändert:       images/polygon-angle.png 	geändert:       n-Eck.rb
    Die Berechnung des regelmäßigen n-Ecks ist die Basis zur Berechnung einer geraden Pyramide mit regelmäßiger Grundfläche.
    Hier: Korrekturen, sinnvolle Begrenzungen (2<n<70.000.000)

--- n-Eck.rb
+++ n-Eck.rb
@@ -3,20 +3,22 @@
 # Polygon mit regelmäßiger Grundfläche
 # n=Anzahl der Ecken, a=Kantenlänge,
 # Berechnung des Innenwinkels zwischen zwei Kanten, gamma (strg+u0263 ->ɣ)
-# Berechnung des Umkreisradius (Strecke C,M), r_u = s
-# Berechnung der Mittelsenkrechten auf a durch M, h_a,M
-
+# Berechnung des Umkreisradius r_u, (Strecke C,M)
 include Math
 
 n = ARGV[0].to_i
 a = ARGV[1].to_f
 	
-if (n == 0.0 or a == 0.0)  and a != n # beide Werte 0, z.B. keine Parameterangabe, dann wird 1.0 gesetzt, s. b==a
+if n == 0.0 or a == 0.0
 puts "n und a müssen ein Wert größer 0 haben!"
 exit
-elsif n == 0 and a == 0 then
-puts "n und a müssen ein Wert größer 0 haben!"
+elsif n < 3
+puts "Ein Polygon hat mindestens 3 Ecken!"
 exit
+elsif n > 70000000
+puts "Es gibt elegantere Möglichkeiten einen Kreis zu konstruieren!"
+exit
+
 end
 
 
@@ -24,20 +26,20 @@ end
 gamma05 = (n-2) * PI * (2*n)**-1
 
 # Strecken
- r_u = (a/2) * cos(gamma05)**-1
- h_aM = sin(gamma05) * r_u
+r_u = (a/2) * cos(gamma05)**-1
+# h_aM = sin(gamma05) * r_u
 
 # Umrechnung Bogenmaß in Grad	
- gamma05 = gamma05 * 180 *PI**-1
- gamma = gamma05*2
+gamma05 = gamma05 * 180 *PI**-1
+gamma = gamma05*2
 
 puts "-----"
 puts "Polygon mit regelmäßiger #{n}-eck-Fläche"
-puts "Skizze s. images/polygon-angle.svg"
+puts "Skizze s. images/polygon-angle.png"
 puts "-----"
 puts "Seitenlänge, a: #{a}"	
 puts "Winkel ɣ: #{gamma.round(5)}"
 puts "-----"
 puts "Radius des Umkreises, r_u: #{r_u.round(5)}"	
-puts "Höhe auf a zu M, h_a,M: #{h_aM.round(5)}"
+#puts "Höhe auf a zu M, h_a,M: #{h_aM.round(5)}"
 puts "-----"
