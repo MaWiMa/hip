4f733cf070d1dc16ca9373399a387dae792be927
MaWiMa <Norbert.Reschke@gMail.com>
2016-06-04 13:59:31 +0200

    geändert:       pmath.rb

--- pmath.rb
+++ pmath.rb
@@ -3,8 +3,11 @@
 # pmath.rb
 # https://github.com/MaWiMa/hip
 # Norbert Reschke
-# Kantenwinkel ω am Grat (hip-angle) einer geraden Pyramide mit regelmäßiger Grundfläche
-# Skizze siehe images/polygon-pyramid-hip-angleGL.png
+# hip (slant edge) angle omega of right pyramid with regular base
+# Kantenwinkel ω am Grat einer geraden Pyramide mit regelmäßiger Grundfläche
+# sketch can be found at:
+# Skizze siehe:
+# images/polygon-pyramid-hip-angleGL.png
 
 require 'matrix'
 include Math
@@ -14,16 +17,16 @@ attr_reader :g,:hg_M, :alpha,:beta,:gamma,:omega, :z,:r,:m, :vec_G,:vec_M,:vec_S
 
 def initialize(n,a,h)
 
-@n = n # number of edges
-@a = a # edge length
-@h = h # height
+@n = n # number of edges, Anzahl der Eckpunkte
+@a = a # edge length, Kantenlänge
+@h = h # height, Höhe
 
 
 @gamma = (n-2) * PI * (2*n)**-1
 @r     = (a/2) * cos(@gamma)**-1
-@beta  = atan(h * @r**-1)
-@alpha = PI*2**-1 -@beta
-@m     = tan(@gamma) * @r
+@beta  = atan(h * @r**-1)          # angle hip to base, Winkel zwischen Grat und Basisebene
+@alpha = PI*2**-1 -@beta           # angle section plane to base, Winkel zwischen Schnittebene und Basisebene 
+@m     = tan(@gamma) * @r          # intersection point(x-axis), Schnittpunkt (X-Richtung); ME1 (+@m) and ME2 (-@m)
 @h_gM  = @r * sin(@beta)
 @omega = atan(@m * @h_gM**-1)
 @z     = sqrt(@h_gM**2 + @m**2)
@@ -32,20 +35,20 @@ def initialize(n,a,h)
 phi  = 2 * PI/n
 r_o  = sin(@beta) * @h_gM
 h_gr = sqrt(@h_gM**2 - r_o**2)
-@g   = sqrt(@r**2 + @h**2)         # Grat der Pyramide
+@g   = sqrt(@r**2 + @h**2)       # hip, Grat
 
 #Schnittebene
 z_E = sin(@alpha) * @r * 1.1
 y_E = cos(@alpha) * @r * 1.1
 
-@vec_M = Vector[0,0,0]           # center of polygon  
-@vec_G = Vector[0,-r_o,h_gr]     # point on hip (g,h_gM)
-@vec_S = Vector[0,0,h]           # top
-@vec_ME1= Vector[@m,0,0]         # Winkelebene Fußpunkt 1
-@vec_ME2= Vector[-@m,0,0]        # Winkelebene Fußpunkt 2
-@vec_GE = Vector[0,-y_E,z_E]     # Winkelebene Mittellinie durch 0 und G
-@vec_GE1 = Vector[@m,-y_E,z_E]   # Winkelebene Oberkantenpunkt 1
-@vec_GE2 = Vector[-@m,-y_E,z_E]  # Winkelebene Oberkantenpunkt 2
+@vec_M = Vector[0,0,0]           # center of polygon, Mittelpunkt  
+@vec_G = Vector[0,-r_o,h_gr]     # point on hip (g,h_gM), Omega Schenkelbasispunkt auf dem Grat
+@vec_S = Vector[0,0,h]           # apex, Spitze
+@vec_ME1= Vector[@m,0,0]         # plane angle, footpoint 1, Winkelebene Fußpunkt 1
+@vec_ME2= Vector[-@m,0,0]        # plane angle, footpoint 2, Winkelebene Fußpunkt 2
+@vec_GE = Vector[0,-y_E,z_E]     # plane angle , Winkelebene Mittellinie durch 0 und G
+@vec_GE1 = Vector[@m,-y_E,z_E]   # Winkelebene Kopfpunkt 1
+@vec_GE2 = Vector[-@m,-y_E,z_E]  # Winkelebene Kopfpunkt 2
 
 base_polygon = [] # create empty array
  for i in (1..n)
@@ -56,50 +59,59 @@ base_polygon = [] # create empty array
  end
 end
 
-
-
-
 def draw_lines
 glLineWidth(2)
 glBegin(GL_LINES)	
 glColor(0.6,0.6,0.6)	
-glVertex(@vec_M.to_a)   # h
+
+# Höhe der Pyramide
+glVertex(@vec_M.to_a)
 glVertex(@vec_S.to_a)	
 
+# Polygonlinien 
 for i in (1..@base.length) # base-area outlines
 glVertex(@base[i-2])
 glVertex(@base[i-1])
 end
 
+# Linien Polygoneckpunkte zum Mittelpunkt
 for i in (1..@base.length) # lines in base-area
 glVertex(@base[i-1])
 glVertex(@vec_M.to_a)	
 end
 
+# Mantelflächenlinien
 for i in (1..@base.length) # lines on outside-surface
 glVertex(@base[i-1])
 glVertex(@vec_S.to_a)	
 end
 
-glColor(0.6,0.6,1.0)	
-glVertex(@vec_ME1.to_a)# Winkelebene Schnittkante an Pyramide, Fußpunkt 2
-glVertex(@vec_G.to_a)  # Winkelebene Schnittkante an Pyramide, Gratpunkt	
+glColor(0.6,0.6,1.0)
+# Winkelebene, Schenkel Omega von Fußpunkt 1 zum Gratpunkt G
+glVertex(@vec_ME1.to_a)
+glVertex(@vec_G.to_a)  	
 
-glVertex(@vec_G.to_a)  # Winkelebene Schnittkante an Pyramide, Gratpunkt	
-glVertex(@vec_ME2.to_a)# Winkelebene Schnittkante an Pyramide, Fußpunkt n
+# Winkelebene, Schenkel Omega von Fußpunkt 2 zum Gratpunkt G
+glVertex(@vec_ME2.to_a)
+glVertex(@vec_G.to_a)  
  
+# Winkelebene, Mittellinie von M zu Kopfpunkt 
 glVertex(@vec_M.to_a)  # Fußpunkt M
-glVertex(@vec_GE.to_a) # Winkelebene Schnittlinie durch den Grat
+glVertex(@vec_GE.to_a) # Schnittpunkt h_gM mit Strecke (GE1-GE2)
 
-glVertex(@vec_ME1.to_a)# Winkelebene Schnittkante an Pyramide, Fußpunkt 2
-glVertex(@vec_ME2.to_a)# Winkelebene Schnittkante an Pyramide, Fußpunkt n
+# Winkelebene, untere Linie von Fußpunkt 1 zu 2 (ME1-M2)
+glVertex(@vec_ME1.to_a)
+glVertex(@vec_ME2.to_a)
 
-glVertex(@vec_GE1.to_a)# Winkelebene 
-glVertex(@vec_GE2.to_a)# Winkelebene 
+# Winkelebene, obere Linie von Kopfpunkt 1 zu 2 (GE1-GE2)
+glVertex(@vec_GE1.to_a)
+glVertex(@vec_GE2.to_a)
 
+# Winkelebene, Seitenlinie von Fußpunkt 2 zu Kopfpunkt 2 (ME2-GE2)
 glVertex(@vec_GE2.to_a)
 glVertex(@vec_ME2.to_a)
 
+# Winkelebene, Seitenlinie von Fußpunkt 1 zu Kopfpunkt 1
 glVertex(@vec_GE1.to_a)
 glVertex(@vec_ME1.to_a)
 
@@ -127,6 +139,7 @@ glVertex(@vec_GE1.to_a)# Winkelebene
 glEnd
 end
 
+=begin
  def draw_triangles
   glBegin(GL_TRIANGLES)
   r,g,b = 244.0/256,164.0/256,96.0/256  # braun
@@ -134,14 +147,11 @@ end
    for i in (1..@base.length)
     glVertex(@base[i-1])
     glVertex(@base[i-2])
-
     glVertex(@vec_S)
-
    end
 glEnd
 end
-
-
+=end
 
 def output
 system "clear"
